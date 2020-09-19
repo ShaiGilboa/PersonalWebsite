880f7149bf30765b947bbb623ac1c6193e9d247f
Shai Gilboa <shizel@gmail.com>
2020-09-19 12:52:57 -0400

    initial

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Under Construction...</h1>
+      <p>for now, you check out my <a href="https://github.com/ShaiGilboa">GitHub</a>, or <a href="https://www.linkedin.com/in/shai-gilboa/" target="__blank">LinkedIn</a></p>
+      <p>Thank!</p>
+    </div>
+  );
+}
+
+export default App;
