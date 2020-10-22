f40e9cd1d084f2b37eebd2a55bb50ea5b7be8084
Shai Gilboa <shizel@gmail.com>
2020-10-21 20:37:30 -0400

    testing

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="App">
       <SEO />
-      <h1>Under Construction...</h1>
+      <h1>Under Construction......</h1>
       <p>for now, you can check out my <a href="https://github.com/ShaiGilboa">GitHub</a>, or <a href="https://www.linkedin.com/in/shai-gilboa/" target="__blank">LinkedIn</a></p>
       <p>Thanks!</p>
     </div>
